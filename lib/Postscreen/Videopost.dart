c5dc2b34dce77b5be3882af0fb23aea62a67154c
Ashh_08 <122357764+AbhishekHeer@users.noreply.github.com>
2023-08-21 19:44:10 +0530

    21/8/23

--- lib/Postscreen/Videopost.dart
+++ lib/Postscreen/Videopost.dart
@@ -1,10 +1,12 @@
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:todo_app/Pages/Toast.dart';
 import 'package:video_player/video_player.dart';
 
@@ -29,70 +31,99 @@ class _VideoPostState extends State<VideoPost> {
     double h = MediaQuery.of(context).size.height;
     double w = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Upload Video',
-          style: TextStyle(fontSize: h * 0.03, fontWeight: FontWeight.w400),
-        ),
-        centerTitle: true,
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: h * 0.16,
-            ),
-            Center(
-              child: InkWell(
-                onTap: () {
-                  setState(() async {
-                    _Videopick();
-                    final upload =
-                        await _db.ref('/Video/$id').putFile(Video!.absolute);
-                    final url = await upload.ref.getDownloadURL();
-
-                    realtimeDB
-                        .child(id)
-                        .set({'id': id, 'Video': url.toString()}).then((value) {
-                      Messege.toast('added');
-                    }).onError((error, stackTrace) {
-                      Messege.toast(error.toString());
-                    });
-                  });
-                },
-                child: Container(
-                    decoration: BoxDecoration(
-                      // color: const Color.fromARGB(255, 179, 96, 206),
-                      borderRadius: BorderRadius.circular(h * 0.05),
-                      border: Border.all(width: w * 0.006),
+      body: Column(
+        children: [
+          Stack(children: [
+            ClipPath(
+              clipper: videoClipper(),
+              child: Container(
+                color: Colors.black,
+                height: h * 0.56,
+                child: Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.topCenter,
+                      child: Text(
+                        'Upload Video',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: h * 0.03,
+                            fontWeight: FontWeight.w400),
+                      ),
                     ),
-                    width: w * 0.8,
-                    height: h * 0.17,
-                    child: Video == null
-                        ? Icon(
-                            CupertinoIcons.videocam,
-                            size: h * 0.06,
-                          )
-                        : VideoPlayer(_controller)),
+                    SizedBox(
+                      height: h * 0.1,
+                    ),
+                    Center(
+                      child: InkWell(
+                        onTap: () async {
+                          await _Videopick();
+                          final upload = await _db
+                              .ref('/Video/$id')
+                              .putFile(Video!.absolute);
+                          final url = await upload.ref.getDownloadURL();
+                          realtimeDB
+                              .child(id)
+                              .set({'id': id, 'Video': url.toString()}).then(
+                                  (value) {
+                            Messege.toast('Added Successfully');
+                          }).onError((error, stackTrace) =>
+                                  Messege.toast(error.toString()));
+                        },
+                        child: Container(
+                            decoration: BoxDecoration(
+                              color: const Color.fromARGB(255, 179, 96, 206),
+                              borderRadius: BorderRadius.circular(h * 0.05),
+                              border: Border.all(width: w * 0.006),
+                            ),
+                            width: w * 0.8,
+                            height: h * 0.17,
+                            child: Video == null
+                                ? Icon(
+                                    CupertinoIcons.videocam,
+                                    size: h * 0.06,
+                                    color: Colors.white,
+                                  )
+                                : VideoPlayer(_controller)),
+                      ),
+                    ),
+                    SizedBox(
+                      height: h * 0.16,
+                    )
+                  ],
+                ),
               ),
-            )
-          ],
-        ),
+            ),
+          ]),
+        ],
       ),
     );
   }
 
-  _Videopick() async {
-    final videopi = await FilePicker.platform.pickFiles(type: FileType.video);
-    setState(() async {
+  Future<void> _Videopick() async {
+    final videopi = await ImagePicker().pickVideo(source: ImageSource.gallery);
+    setState(() {
       if (videopi == null) {
         return;
       } else {
-        var finalurl = videopi.files.first as File?;
-
-        Video = finalurl!.absolute;
+        Video = File(videopi.path);
       }
     });
   }
+}
+
+class videoClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, size.height - 100);
+    path.lineTo(size.width / 2, size.height);
+    path.lineTo(size.width, size.height - 100);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:lottie/lottie.dart';
 import 'package:todo_app/Pages/BottomNavi.dart';
-import 'package:todo_app/Pages/Camera.dart';
 import 'package:todo_app/Postscreen/Videopost.dart';
-import 'package:todo_app/Profile/Profile.dart';
 import 'package:todo_app/files/Auth/Login.dart';
 import 'firebase_options.dart';
 
@@ -42,7 +40,7 @@ class MyApp extends StatelessWidget {
               seedColor: const Color.fromARGB(255, 153, 150, 158)),
           useMaterial3: true,
         ),
-        home: const Splash());
+        home: const VideoPost());
   }
 }
 
@@ -56,7 +54,6 @@ class Splash extends StatefulWidget {
 final _db = FirebaseAuth.instance;
 
 class _SplashState extends State<Splash> {
-  @override
   @override
   void initState() {
     super.initState();
